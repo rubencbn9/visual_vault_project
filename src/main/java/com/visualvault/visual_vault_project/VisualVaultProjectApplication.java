e99cf2488504c55dd926cd7ac2d8c0c05e270ff3
rubencb9 <ruben.barreiro.7@gmail.com>
2025-09-26 16:17:15 +0200

    primer commit

---
+++ src/main/java/com/visualvault/visual_vault_project/VisualVaultProjectApplication.java
@@ -0,0 +1,13 @@
+package com.visualvault.visual_vault_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualVaultProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualVaultProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visualvault/visual_vault_project/VisualVaultProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visualvault.visual_vault_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualVaultProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
